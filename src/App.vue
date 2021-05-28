140cece91a1a9fc266d3196b73ee5093df1b730c
Caio Guilherme <57760300+CaioGui123@users.noreply.github.com>
2021-05-27 21:44:07 -0300

    :tada: - Initial Commit and Home Page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@500;700&display=swap');
+
+:root {
+  --yellow: #FCC003;
+  --dark-yellow: #E0AF15;
+  --orange: #EE7B01;
+  --brown: #2B1106;
+  --black: #232323;
+  --white: #FFFFFF;
+
+  font-size: 62.5%;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  font-size: 1.6rem;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+* {
+  font-family: 'Jost', sans-serif;
+  font-weight: 500;
+  color: var(--black);
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+input, textarea, button {
+  font-family:  'Jost', sans-serif;
+  font-weight: 500;
+  color: var(--black);
+}
+
+button {
+  cursor: pointer;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+      <div class="card-header">
+        <h1 class="title">Descubra e mostre o seu mundo para a humanidade!</h1>
+        <p class="subtitle"><em>"Deixe prevalecer aquilo que te dá forças."</em></p>
+      </div>
+
+      <div class="card-footer">
+        <router-link :to="{ name: 'login' }" class="sign-in">Login</router-link>
+
+        <router-link :to="{ name: 'register' }" class="sign-up">Cadastrar-se</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  width: 100vw;
+  background: url('~@/assets/images/home-bg.jpg') no-repeat;
+  background-size: cover;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  background: var(--white);
+  padding: 2.5rem;
+  width: 55rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  .card-header {
+    .title {
+      font-size: 4.8rem;
+      font-weight: bold;
+      line-height: 4rem;
+      color: var(--orange);
+    }
+    .subtitle {
+      margin-top: 1rem;
+    }
+  }
+
+  .card-footer {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    a.sign-in {
+      background: var(--yellow);
+      font-weight: bold;
+      padding: 1.5rem 8rem;
+      border: none;
+      border-radius: .5rem;
+      margin-right: 1.5rem;
+      transition: all .2s;
+
+      &:hover {
+        background: var(--dark-yellow);
+      }
+    }
+    a.sign-up {
+      background: transparent;
+      font-weight: bold;
+      color: var(--yellow);
+      padding: 1.5rem 8rem;
+      border: 2px solid var(--yellow);
+      border-radius: .5rem;
+      transition: all .2s;
+
+      &:hover {
+        background: var(--yellow);
+        color: var(--black);
+      }
+    }
+  }
+}
+</style>
